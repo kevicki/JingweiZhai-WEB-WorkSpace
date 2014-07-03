a9a2fb949592ed4a697d5b50b07ed28bb430c863
Jingwei Zhai <kevicki@gmail.com>
2014-07-03 22:37:50 +0800

    A new empty ASP.Net project
    The first start!

---
+++ workSpace/MY WEB GIS/MY WEB GIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MY_WEB_GIS.MvcApplication" Language="C#" %>
---
+++ workSpace/MY WEB GIS/MY WEB GIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MY_WEB_GIS.Startup))]
+namespace MY_WEB_GIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

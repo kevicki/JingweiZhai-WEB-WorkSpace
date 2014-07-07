b16697717e2c0c7138c7f01474722630c95de783
Jingwei Zhai <kevicki@gmail.com>
2014-07-07 21:26:34 +0800

    first js file

---
+++ workSpace/MY WEB GIS/MY WEB GIS/Scripts/projectJS/GISMap.js
@@ -0,0 +1,15 @@
+﻿function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+};
+
+Person.prototype = {
+    constuctor: Person,
+    sayName: function () {
+        alert(this.name);
+    }
+};
+
+var person1 = new Person("jingwei", "18", "Engineer");
+person1.sayName();
